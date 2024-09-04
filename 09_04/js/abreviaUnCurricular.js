685823bf73e0bb7e8510965b0c862f9d0761f37f
Amanda Xavier <amanda.x.nascimento@aluno.senai.br>
2024-09-04 17:08:20 -0300

    Abreviando nomes-monitor

---
+++ 09_04/js/abreviaUnCurricular.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function abreviaUnCurricular (unidade_curricular) {
+    let uc = unidade_curricular.split(' ');
+    uc.splice(-2);
+    uc = uc[0].substring(4,0)+'. ' + uc[uc.length -1];
+    return uc;
+}
+
+
+export default abreviaUnCurricular;
